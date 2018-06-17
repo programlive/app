2035ae85be695383d889db77f5c3a85815305d21
ji chuan huang <programlive@outlook.com>
2018-06-17 14:17:17 +0800

    Update TJscript.js

--- TJscript.js
+++ TJscript.js
@@ -1,11 +1,8 @@
+
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?619a502eb96f2f12257ece4bf59960ca";
+  hm.src = "https://hm.baidu.com/hm.js?c956e0b86f2d6faa92567fbe4a46c956";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-console.log("ok");
-
-
-} 
